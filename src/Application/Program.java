0d3ba9ff0b291af72dd3570fdcf19920b5edbc38
ErnayVict <ernany12victor@gmail.com>
2025-04-07 01:13:10 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,12 +1,19 @@
 package Application;
 
+import java.util.Date;
+
 import entities.Departament;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departament obj = new Departament(1, "Books");
+
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
