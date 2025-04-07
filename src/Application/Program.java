9b35b7ce555490fe125965b929791f6763118e67
ErnayVict <ernany12victor@gmail.com>
2025-04-07 01:01:22 -0300

    Departament entity class

---
+++ src/Application/Program.java
@@ -0,0 +1,13 @@
+package Application;
+
+import entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departament obj = new Departament(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
